25835d84818e14a4cd3b7ea0f52518945316a39c
Martin Kagamino Lehoux <martin@lehoux.net>
2022-02-25 12:40:53 +0100

    feat: Draw a sky

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn mix_taint(taint_1: f64, taint_2: f64, ratio: f64) -> f64 {
     taint_1 * ratio + taint_2 * (1.0 - ratio)
 }
 
+#[derive(Clone)]
 struct Color {
     red: f64,
     green: f64,
@@ -72,7 +73,12 @@ impl Image {
         Ok(())
     }
 
-    fn sample() -> Image {
+    fn sample(
+        origin: Point3,
+        lower_left_corner: Point3,
+        horizontal: Vec3,
+        vertical: Vec3,
+    ) -> Image {
         let mut image = Image {
             width: 1920,
             height: 1080,
@@ -81,11 +87,16 @@ impl Image {
         for y in 0..image.height {
             let mut line = Vec::<Color>::new();
             for x in 0..image.width {
-                line.push(Color {
-                    red: (x as f64 / (image.width - 1) as f64),
-                    green: (y as f64 / (image.height - 1) as f64),
-                    blue: 0.25,
-                })
+                let u = x as f64 / (image.width - 1) as f64;
+                let v = y as f64 / (image.height - 1) as f64;
+                let ray = Ray {
+                    origin: origin.clone(),
+                    direction: lower_left_corner.clone()
+                        + horizontal.clone() * u
+                        + vertical.clone() * v
+                        - origin.clone(),
+                };
+                line.push(Ray::color(ray));
             }
             image.lines.push(line);
         }
@@ -93,6 +104,7 @@ impl Image {
     }
 }
 
+#[derive(Clone)]
 struct Vec3 {
     x: f64,
     y: f64,
@@ -183,12 +195,59 @@ impl Ray {
     fn at(self, t: Time) -> Point3 {
         self.origin + self.direction * t
     }
+
+    fn color(ray: Ray) -> Color {
+        let t = 0.5 * (ray.direction.unit().y + 1.0);
+        Color::mix(
+            Color {
+                red: 0.5,
+                green: 0.7,
+                blue: 1.0,
+            },
+            WHITE,
+            t,
+        )
+    }
 }
 
 fn main() {
     println!("Hello, world!");
 
-    let image = Image::sample();
+    let aspect_ratio = 16.0 / 9.0;
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * aspect_ratio;
+    let focal_length = 1.0;
+
+    let origin = Point3 {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+    };
+    let horizontal = Vec3 {
+        x: viewport_width,
+        y: 0.0,
+        z: 0.0,
+    };
+    let vertical = Vec3 {
+        x: 0.0,
+        y: viewport_height,
+        z: 0.0,
+    };
+    let lower_left_corner = origin.clone()
+        - horizontal.clone() * 0.5
+        - vertical.clone() * 0.5
+        - Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: focal_length,
+        };
+
+    let image = Image::sample(
+        origin.clone(),
+        lower_left_corner,
+        horizontal.clone(),
+        vertical.clone(),
+    );
     let file = fs::File::create("test.ppm").unwrap();
     let mut buffer = io::BufWriter::new(file);
     image.draw(&mut buffer).unwrap();
